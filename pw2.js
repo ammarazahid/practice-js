347fe459303e5ee276bf325269ee716e44fdd6ab
ammarazahid <49521489+ammarazahid@users.noreply.github.com>
2019-07-31 23:00:36 +0500

    Add files via upload

--- pw2.js
+++ pw2.js
@@ -1,144 +1,144 @@
-// let videos = [{
-//         title: "Photoshop tutorial",
-//         lengthInMinutes: 70,
-//         category: "Education",
-//         uploadDate: new Date("07-22-2019"),
-//         tags: "design, digital, photoshop, creativity",
-//         features: ["Live", "360°", "HDR"],
-//         viewCount: 4700,
-//         rating: 4.3
-//     },
-//     {
-//         title: "Episode # 01 - The Best Comedy Show",
-//         lengthInMinutes: 19,
-//         category: "Entertainment",
-//         uploadDate: new Date("07-03-2019"),
-//         tags: "comedy, funny",
-//         features: ["Subtitles/CC", "3D", "HD"],
-//         viewCount: 145615,
-//         rating: 3.9
-//     },
-//     {
-//         title: "How to use FOR EACH loop - tutorial by Tech Karo",
-//         lengthInMinutes: 6,
-//         category: "Education",
-//         uploadDate: new Date("11-10-2018"),
-//         tags: "javascript, loops, web development",
-//         features: ["Purchased", "HD"],
-//         viewCount: 9004,
-//         rating: 4.5
-//     },
-//     {
-//         title: "How to use css",
-//         lengthInMinutes: 20,
-//         category: "Education",
-//         uploadDate: new Date("10-24-2017"),
-//         tags: "style, background, hover",
-//         features: ["purchased", "HDR"],
-//         viewCount: 3034,
-//         rating: 4.4
-//     },
-//     {
-//         title: "Loops Tutorial",
-//         lengthInMinutes: 25,
-//         category: "Education",
-//         uploadDate: new Date("9-22-2017"),
-//         tags: "for loop, each loop, while loop",
-//         features: ["Purchased", "HD"],
-//         viewCount: 4540,
-//         rating: 4.1
-//     },
-//     {
-//         title: "Biryani Recipe - Tutorial by Food Fusion",
-//         lengthInMinutes: 10,
-//         category: "Recipe",
-//         uploadDate: new Date("11-29-2019"),
-//         tags: "ingredients,  New recipes, Korma",
-//         features: ["Live", "HD"],
-//         viewCount: 3456,
-//         rating: 4.5
-//     },
-//     {
-//         title: "Episode # 2 - The best comedy show",
-//         lengthInMinutes: 13,
-//         category: "Entertainment",
-//         uploadDate: new Date("12-29-2018"),
-//         tags: "comedy, funny",
-//         features: ["Live", "HDR"],
-//         viewCount: 3200,
-//         rating: 4.3
-//     },
-//     {
-//         title: "Motivation Video Funny HD",
-//         lengthInMinutes: 4,
-//         category: "Motivation",
-//         uploadDate: new Date("2-16-2018"),
-//         tags: "motivational, funny",
-//         features: ["Live", "Relevance"],
-//         viewCount: 2000,
-//         rating: 4.4
-//     },
-//     {
-//         title: "World Best Motivational Videos for Students",
-//         lengthInMinutes: 3,
-//         category: "Motivation",
-//         uploadDate: new Date("12-29-2017"),
-//         tags: "motivational, funny",
-//         features: ["Live", "HDR"],
-//         viewCount: 2000,
-//         rating: 4.2
-//     },
-//     {
-//         title: "Doremon - Kids Cartoon",
-//         lengthInMinutes: 30,
-//         category: "Entertainment",
-//         uploadDate: new Date("8-27-2018"),
-//         tags: "funny, comedy",
-//         features: ["Live", "360°", "HDR"],
-//         viewCount: 1200,
-//         rating: 4.1
-//     },
-//     {
-//         title: "herry Porter",
-//         lengthInMinutes: 49,
-//         category: "Entertainment",
-//         uploadDate: new Date("6-13-2019"),
-//         tags: "funny, entertainments, enjoyable",
-//         features: ["Live", "HD"],
-//         viewCount: 5000,
-//         rating: 4.6
-//     },
-//     {
-//         title: "For each loop tutorials",
-//         lengthInMinutes: 30,
-//         category: "Education",
-//         uploadDate: new Date("7-27-2019"),
-//         tags: "javascript, loops, web development",
-//         features: ["Live", "360°", "HDR"],
-//         viewCount: 300,
-//         rating: 4.3
-//     },
-//     {
-//         title: "Seesaw - baby toys - have fun in the park",
-//         lengthInMinutes: 14,
-//         category: "funny",
-//         uploadDate: new Date("14-14-2014"),
-//         tags: "kids  fun, hero, ",
-//         features: ["Live", "360°", "HDR"],
-//         viewCount: 6000,
-//         rating: 4.4
-//     },
-//     {
-//         title: "Array methods",
-//         lengthInMinutes: 13,
-//         category: "Education",
-//         uploadDate: new Date("12-26-2019"),
-//         tags: "javascript, arrays, webdevelopment",
-//         features: ["Live", "360°", "HDR"],
-//         viewCount: 5700,
-//         rating: 4.4
-//     }
-// ];
+let videos = [{
+        title: "Photoshop tutorial",
+        lengthInMinutes: 70,
+        category: "Education",
+        uploadDate: new Date("07-22-2019"),
+        tags: "design, digital, photoshop, creativity",
+        features: ["Live", "360°", "HDR"],
+        viewCount: 4700,
+        rating: 4.3
+    },
+    {
+        title: "Episode # 01 - The Best Comedy Show",
+        lengthInMinutes: 19,
+        category: "Entertainment",
+        uploadDate: new Date("07-03-2019"),
+        tags: "comedy, funny",
+        features: ["Subtitles/CC", "3D", "HD"],
+        viewCount: 145615,
+        rating: 3.9
+    },
+    {
+        title: "How to use FOR EACH loop - tutorial by Tech Karo",
+        lengthInMinutes: 6,
+        category: "Education",
+        uploadDate: new Date("11-10-2018"),
+        tags: "javascript, loops, web development",
+        features: ["Purchased", "HD"],
+        viewCount: 9004,
+        rating: 4.5
+    },
+    {
+        title: "How to use css",
+        lengthInMinutes: 20,
+        category: "Education",
+        uploadDate: new Date("7-31-2019"),
+        tags: "style, background, hover",
+        features: ["purchased", "HDR"],
+        viewCount: 3034,
+        rating: 4.4
+    },
+    {
+        title: "Loops Tutorial",
+        lengthInMinutes: 25,
+        category: "Education",
+        uploadDate: new Date("9-22-2017"),
+        tags: "for loop, each loop, while loop",
+        features: ["Purchased", "HD"],
+        viewCount: 4540,
+        rating: 4.1
+    },
+    {
+        title: "Biryani Recipe - Tutorial by Food Fusion",
+        lengthInMinutes: 10,
+        category: "Recipe",
+        uploadDate: new Date("11-29-2019"),
+        tags: "ingredients,  New recipes, Korma",
+        features: ["Live", "HD"],
+        viewCount: 3456,
+        rating: 4.5
+    },
+    {
+        title: "Episode # 2 - The best comedy show",
+        lengthInMinutes: 13,
+        category: "Entertainment",
+        uploadDate: new Date("12-29-2018"),
+        tags: "comedy, funny",
+        features: ["Live", "HDR"],
+        viewCount: 3200,
+        rating: 4.3
+    },
+    {
+        title: "Motivation Video Funny HD",
+        lengthInMinutes: 4,
+        category: "Motivation",
+        uploadDate: new Date("2-16-2018"),
+        tags: "motivational, funny",
+        features: ["Live", "Relevance"],
+        viewCount: 2000,
+        rating: 4.4
+    },
+    {
+        title: "World Best Motivational Videos for Students",
+        lengthInMinutes: 3,
+        category: "Motivation",
+        uploadDate: new Date("12-29-2017"),
+        tags: "motivational, funny",
+        features: ["Live", "HDR"],
+        viewCount: 2000,
+        rating: 4.2
+    },
+    {
+        title: "Doremon - Kids Cartoon",
+        lengthInMinutes: 30,
+        category: "Entertainment",
+        uploadDate: new Date("8-27-2018"),
+        tags: "funny, comedy",
+        features: ["Live", "360°", "HDR"],
+        viewCount: 1200,
+        rating: 4.1
+    },
+    {
+        title: "herry Porter",
+        lengthInMinutes: 49,
+        category: "Entertainment",
+        uploadDate: new Date("6-13-2019"),
+        tags: "funny, entertainments, enjoyable",
+        features: ["Live", "HD"],
+        viewCount: 5000,
+        rating: 4.6
+    },
+    {
+        title: "For each loop tutorials",
+        lengthInMinutes: 30,
+        category: "Education",
+        uploadDate: new Date("7-27-2019"),
+        tags: "javascript, loops, web development",
+        features: ["Live", "360°", "HDR"],
+        viewCount: 300,
+        rating: 4.3
+    },
+    {
+        title: "Seesaw - baby toys - have fun in the park",
+        lengthInMinutes: 14,
+        category: "funny",
+        uploadDate: new Date("7-14-2014"),
+        tags: "kids  fun, hero, ",
+        features: ["Live", "360°", "HDR"],
+        viewCount: 6000,
+        rating: 4.4
+    },
+    {
+        title: "Array methods",
+        lengthInMinutes: 13,
+        category: "Education",
+        uploadDate: new Date("12-26-2019"),
+        tags: "javascript, arrays, webdevelopment",
+        features: ["Live", "360°", "HDR"],
+        viewCount: 5700,
+        rating: 4.4
+    }
+];
 
 
 
@@ -146,14 +146,13 @@
 
 
 // for (let i = 0; i < videos.length; i++){
-// // //     console.log("Title: " + videos[i].title);
-// // //     console.log("Length: " + videos[i].lengthInMinutes + "minutes");
-// // //     console.log("Category: " + videos[i].category);
-// // //     console.log("Views: " + videos[i].viewCount.toLocaleString());
-// console.log("Views: " + videos[i].viewCount.toLocaleString());
-// // //     console.log("Uploaded On: " + videos[i].uploadDate.getMonth() + "-" + videos[i].uploadDate.getDate() + "-" + videos[i].uploadDate.getFullYear());
-// // //     console.log("Rating: ");
-// // //     console.log(" ");
+//     console.log("Title: " + videos[i].title);
+//     console.log("Length: " + videos[i].lengthInMinutes + "minutes");
+//     console.log("Category: " + videos[i].category);
+//     console.log("Views: " + videos[i].viewCount.toLocaleString());
+//     console.log("Uploaded On: " + videos[i].uploadDate.getDate() + "-" + videos[i].uploadDate.getMonth() + "-" + videos[i].uploadDate.getFullYear());
+//     console.log("Rating: ");
+//     console.log(" ");
 // }
 
 
@@ -198,31 +197,30 @@ for (let i = 0; i < videos.length; i++) {
 // }
 
 //  6.
-/*
-for(let i = 0; i < videos.length; i++){
-    if(videos[i].category == "Education"){
-        console.log("Category: " + videos[i].title);
-        console.log("");
-    }
-}
 
-*/
+// for(let i = 0; i < videos.length; i++){
+//     if(videos[i].category == "Education"){
+//         console.log("Category: " + videos[i].title);
+//         console.log("");
+//     }
+// }
+
 
 // 7.
 
-// for (let i = 0; i < videos.length; i++){
-// if(videos[i].tags.slice() == "javascript") {
-// console.log(videos[i].title);
+// for (let i = 0; i < videos.length; i++) {
+//     if (videos[i].tags.includes("javascript")) {
+//         console.log(videos[i].title);
+//     }
 // }
-// }
 
 
 
-//  8.
+//  8. Incomplete
 
 // for(let i = 0; i < videos.length; i++){
-//     if(videos[i].features === "HD"){
-//         console.log(Videos[i].title);
+//     if(videos[i].features.indexOf("HD")){
+//         console.log(videos[i].title);
 //         console.log("");
 //     }
 // }
@@ -232,20 +230,30 @@ for(let i = 0; i < videos.length; i++){
 
 
 // // //  9.
-// let today =new Date();
+// let today = new Date();
 // for(let i = 0; i < videos.length; i++){
-//    if(videos[i].uploadDate.getMonth() + "-" + videos[i].uploadDate.getDate() + "-" + videos[i].uploadDate.getFullYear() === today){
+//    if(videos[i].uploadDate.getDate() + "-" + videos[i].uploadDate.getMonth() + "-" + videos[i].uploadDate.getFullYear()  == today.getDate() + "-" + today.getMonth() + "-" + today.getFullYear()){
 //       console.log(videos[i].title);
 // }
 // }
 
 
-// 11.
+//  10.Incomplete
 
-// let today = new Date().getMonth();
+
+// for(let i = 0; i < videos.length; i++) {
+//     if(videos[i].uploadDate.getDay() > 7){
+
+//     }
+// }
+
+
+// 11. plus kr rha hai doremon ko bhi
+
+// let today = new Date();
 
 // for (let i = 0; i < videos.length; i++) {
-//     if (videos[i].uploadDate.getMonth() == today) {
+//     if (videos[i].uploadDate.getMonth() + videos[i].uploadDate.getFullYear() == today.getMonth() + today.getFullYear()) {
 //         console.log(videos[i].title);
 //     }
 // }
@@ -261,11 +269,10 @@ for(let i = 0; i < videos.length; i++){
 //     }
 // }
 
-//  13.
+//  13. incomplete
 
 // for(let i = 0; i < videos.length; i++) {
-//     videos..sort()
-// console.log(videos[i].title);
+// console.log(videos[i].viewCount.sort(function));
 // console.log("");
 
 // }
--- script.js
+++ script.js
@@ -1,94 +1,94 @@
-let students = [
-    {
-        name: "Amna",
-        gender: "f",
-        dob: new Date("02-04-1990"),
-        address: {
-            ilaqa: "Gulistan-e-Johar",
-            city: "Karachi",
-            country: "Pakistan",
-            postalCode: 47114
-        },
-        phoneNo: "0331-2324243",
-        admissionTestScore: 56,
-        hasInternet: true,
-        hasComputer: false,
-        hasJob: true,
-        hasSchoolBefore: false
-    },
-    {
-        name: "Hadia",
-        gender: "f",
-        dob: new Date("05-15-1984"),
-        address: {
-            ilaqa: "Lyari",
-            city: "Karachi",
-            country: "Pakistan",
-            postalCode: 75660
-        },
-        phoneNo: "0345-3452953",
-        admissionTestScore: 48,
-        hasInternet: false,
-        hasComputer: false,
-        hasJob: false,
-        hasSchoolBefore: true
-    },
-    {
-        name: "Ahmed",
-        gender: "m",
-        dob: new Date("06-27-2002"),
-        address: {
-            ilaqa: "University Road",
-            city: "Quetta",
-            country: "Pakistan",
-            postalCode: 82215
-        },
-        phoneNo: "0333-0124325",
-        admissionTestScore: 33,
-        hasInternet: true,
-        hasComputer: false,
-        hasJob: false,
-        hasSchoolBefore: false
-    },
-    {
-        name: "Fariha",
-        gender: "f",
-        dob: new Date("09-13-1998"),
-        address: {
-            ilaqa: "University Road",
-            city: "Karachi",
-            country: "Pakistan",
-            postalCode: 82215
-        },
-        phoneNo: "0331-9432532",
-        admissionTestScore: 33,
-        hasInternet: true,
-        hasComputer: false,
-        hasJob: false,
-        hasSchoolBefore: false
-    },
-    {
-        name: "Abdullah",
-        gender: "m",
-        dob: new Date("01-24-1972"),
-        address: {
-            ilaqa: "Bazar Colony",
-            city: "Lahore",
-            country: "Pakistan",
-            postalCode: 32212
-        },
-        phoneNo: "0345-9912121",
-        admissionTestScore: 33,
-        hasInternet: false,
-        hasComputer: false,
-        hasJob: true,
-        hasSchoolBefore: true
-    },
-    {
-        name: "Ramsha",
-        dob: new Date("7-23-1997")
-    }
-];
+// let students = [
+//     {
+//         name: "Amna",
+//         gender: "f",
+//         dob: new Date("02-04-1990"),
+//         address: {
+//             ilaqa: "Gulistan-e-Johar",
+//             city: "Karachi",
+//             country: "Pakistan",
+//             postalCode: 47114
+//         },
+//         phoneNo: "0331-2324243",
+//         admissionTestScore: 56,
+//         hasInternet: true,
+//         hasComputer: false,
+//         hasJob: true,
+//         hasSchoolBefore: false
+//     },
+//     {
+//         name: "Hadia",
+//         gender: "f",
+//         dob: new Date("05-15-1984"),
+//         address: {
+//             ilaqa: "Lyari",
+//             city: "Karachi",
+//             country: "Pakistan",
+//             postalCode: 75660
+//         },
+//         phoneNo: "0345-3452953",
+//         admissionTestScore: 48,
+//         hasInternet: false,
+//         hasComputer: false,
+//         hasJob: false,
+//         hasSchoolBefore: true
+//     },
+//     {
+//         name: "Ahmed",
+//         gender: "m",
+//         dob: new Date("06-27-2002"),
+//         address: {
+//             ilaqa: "University Road",
+//             city: "Quetta",
+//             country: "Pakistan",
+//             postalCode: 82215
+//         },
+//         phoneNo: "0333-0124325",
+//         admissionTestScore: 33,
+//         hasInternet: true,
+//         hasComputer: false,
+//         hasJob: false,
+//         hasSchoolBefore: false
+//     },
+//     {
+//         name: "Fariha",
+//         gender: "f",
+//         dob: new Date("09-13-1998"),
+//         address: {
+//             ilaqa: "University Road",
+//             city: "Karachi",
+//             country: "Pakistan",
+//             postalCode: 82215
+//         },
+//         phoneNo: "0331-9432532",
+//         admissionTestScore: 33,
+//         hasInternet: true,
+//         hasComputer: false,
+//         hasJob: false,
+//         hasSchoolBefore: false
+//     },
+//     {
+//         name: "Abdullah",
+//         gender: "m",
+//         dob: new Date("01-24-1972"),
+//         address: {
+//             ilaqa: "Bazar Colony",
+//             city: "Lahore",
+//             country: "Pakistan",
+//             postalCode: 32212
+//         },
+//         phoneNo: "0345-9912121",
+//         admissionTestScore: 33,
+//         hasInternet: false,
+//         hasComputer: false,
+//         hasJob: true,
+//         hasSchoolBefore: true
+//     },
+//     {
+//         name: "Ramsha",
+//         dob: new Date("7-23-1997")
+//     }
+// ];
 
 // for (let i = 0; i < students.length; i++) {
 //    if(students[i].phoneNo.startsWith("033")) {
@@ -127,32 +127,32 @@ Amna's age is 29 years
 
 //  9.
 
-function calculateAge(dateOfoBirth){
-    let year = dateOfoBirth.getFullYear();
-    let today = new Date().getFullYear();
+// function calculateAge(dateOfoBirth){
+//     let year = dateOfoBirth.getFullYear();
+//     let today = new Date().getFullYear();
 
-    let age = today - year;
+//     let age = today - year;
 
-    let month = dateOfoBirth.getMonth();
-    let todayMonth = new Date().getMonth();
+//     let month = dateOfoBirth.getMonth();
+//     let todayMonth = new Date().getMonth();
 
-    if (todayMonth < month) {
-        age = age - 1;
-    } else if (todayMonth === month) {
-        let todayDate = new Date().getDate();
-        let dobDate = dateOfoBirth.getDate();
-        if (todayDate < dobDate){
-            age = age - 1;
-        }
-    return age;
-}   
-}
+//     if (todayMonth < month) {
+//         age = age - 1;
+//     } else if (todayMonth === month) {
+//         let todayDate = new Date().getDate();
+//         let dobDate = dateOfoBirth.getDate();
+//         if (todayDate < dobDate){
+//             age = age - 1;
+//         }
+//     return age;
+// }   
+// }
 
 // 10.
 
-for(let i = 0; i < students.length; i++){
-    let studentAge =calculateAge
-}
+// for(let i = 0; i < students.length; i++){
+//     let studentAge =calculateAge
+// }
 
 //  let oldestStudent = students[0];
 
